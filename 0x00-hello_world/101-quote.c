9a81ae591e33f6f4b7eae986b5c1737b0b38ee7c
Barrel Zegamon <israelbassey.ib@gmail.com>
2023-03-17 07:20:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,19 @@
 #include <unistd.h>
 
-int main(void) {
-  char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, message, 41);
-  return 1;
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int len = 0;
+
+    while (str[len])
+        len++;
+
+    write(2, str, len);
+
+    return (1);
 }
