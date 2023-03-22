340902a53bc680bf93a6f125f5cb4d210cf5937e
Barrel Zegamon <israelbassey.ib@gmail.com>
2023-03-22 05:50:29 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase followed by a new line
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void
-print_alphabet (void)
+void print_alphabet(void)
 {
-  char c = 'a';
+	char letter;
 
-  while (c <= 'z')
-    {
-      _putchar (c);
-      c++;
-    }
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-  _putchar ('\n');
+	_putchar('\n');
 }
